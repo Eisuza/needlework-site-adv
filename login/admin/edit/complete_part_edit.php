5eb9f02107c85a3c611383f6d5b89327ad907c54
eisuza <78528013+Eisuza@users.noreply.github.com>
2023-06-23 22:58:06 +0300

    Create complete_part_edit.php

---
+++ login/admin/edit/complete_part_edit.php
@@ -0,0 +1,8 @@
+<div>
+    <p class="text_right">Администратор: <?=$_SESSION['user'];?>
+    <br><a href= "../../../logout.php" class="text_right">Выход</a></p>
+</div>
+<div class="form">
+    <p class="text_color">Редактирование завершено <?=$result_phrase?></p>
+    <p><a href="../../../admin.php" class="form_button">Вернуться</a></p>
+</div>
