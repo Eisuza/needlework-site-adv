1d02376a1a272b733cc67b0c3b2b8b0b4157ef33
eisuza <78528013+Eisuza@users.noreply.github.com>
2023-06-23 22:58:32 +0300

    Create admin_mail_conf.php

---
+++ login/admin_mail_conf.php
@@ -0,0 +1,14 @@
+<?php
+$mail_settings = [
+	'host' => 'smtp.mailtrap.io',
+	'auth' => true,
+	'port' => 2525,
+	'username' => '22514b9e12bb49',
+	'password' => 'cd1bf92aa51f6f',
+	'secure' => null,
+	'charset' => 'UTF-8',
+	'from' => '027af43025-35bcaa@inbox.mailtrap.io',
+	'name' => 'RukodelieSite',
+	'is_html' => true,
+];
+?>
