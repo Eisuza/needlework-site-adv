271793861db7cd0e55484119395c72fe6978fbbc
eisuza <78528013+Eisuza@users.noreply.github.com>
2023-06-23 22:58:36 +0300

    Create processing.php

---
+++ login/processing.php
@@ -0,0 +1,9 @@
+<?php
+require_once("../func/db_connection.php");
+require_once("../func/login.php");
+$login=$_POST["login"];
+$password=$_POST["password"];
+
+$link = connect_to_db();
+login($link, $login, $password);
+ ?>
