f1fde8d76f3538b36fb7a6a5ea792b582101864d
eisuza <78528013+Eisuza@users.noreply.github.com>
2023-06-23 22:58:22 +0300

    Create logout_part_edit.php

---
+++ login/admin/edit/logout_part_edit.php
@@ -0,0 +1,6 @@
+<div>
+    <p class="text_right">Администратор: <?=$_SESSION['user'];?>
+    <br><a href= "../../../logout.php" class="text_right">Выход</a></p>
+    <p class="caption">Редактирование элемента.
+    <br><a href="../../../admin.php" class="text_color">Вернуться</a></p>
+</div>
