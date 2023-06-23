1703c6e86e556dde83892a5644603191c312fe83
eisuza <78528013+Eisuza@users.noreply.github.com>
2023-06-23 22:58:19 +0300

    Create header_part_edit.php

---
+++ login/admin/edit/header_part_edit.php
@@ -0,0 +1,6 @@
+    <head>
+        <title>Редактирование.</title>
+        <link rel="stylesheet" href="../admin.css">
+        <link rel="icon" href="../admin.ico" type="image/x-icon">
+        <meta charset="UTF-8">
+    </head>
