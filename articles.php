f4a2bf964d5c1d956213eb10f7b5d3977bd5eff9
eisuza <78528013+Eisuza@users.noreply.github.com>
2023-06-23 22:56:03 +0300

    Create articles.php

---
+++ articles.php
@@ -0,0 +1,19 @@
+<?php
+    require_once("func/db_connection.php");
+    require_once("func/page_data.php");
+    require_once("func/put_content.php");
+   
+    $link = connect_to_db();
+    $id_page = 3;
+
+    $content = "";
+    $templates = get_templates($link, $id_page);
+    foreach($templates as $template):
+        $raw_content = file_get_contents("$template[link]");
+        $content .= put_content($link, $raw_content, $template['id_template'], $id_page);
+    endforeach;
+
+    file_put_contents("temp", $content);
+    include "temp";
+    unlink("temp");
+?>
